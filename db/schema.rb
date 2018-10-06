572a985516776b32fe26f66d962833b238f1ee84
Pranay Singh <pranay.singh@credihealth.com>
2018-10-06 15:45:26 +0530

    added authorization and association for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181006094718) do
+ActiveRecord::Schema.define(version: 20181006095702) do
 
   create_table "links", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20181006094718) do
     t.boolean "hide", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
