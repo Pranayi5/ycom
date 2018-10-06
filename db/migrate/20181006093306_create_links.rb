c8bec4c14f119d63b15e101ebe40093d9a695021
Pranay Singh <pranay.singh@credihealth.com>
2018-10-06 15:10:57 +0530

    link scaffold with intial hide value false

---
+++ db/migrate/20181006093306_create_links.rb
@@ -0,0 +1,11 @@
+class CreateLinks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :links do |t|
+      t.string :title
+      t.string :url
+      t.boolean :hide, default: false
+
+      t.timestamps
+    end
+  end
+end
